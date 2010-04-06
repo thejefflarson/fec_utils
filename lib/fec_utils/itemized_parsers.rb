2bbe8e49034a6270caf5f94b9a2c3ec3a5e31399
Jeff Larson <thejefflarson@gmail.com>
2010-04-06 15:09:54 -0400

    this thing should have a version after all

--- lib/fec_utils/itemized_parsers.rb
+++ lib/fec_utils/itemized_parsers.rb
@@ -5,6 +5,5 @@ module FecUtils
     require 'fec_utils/itemized_parsers/candidate_parse'
     require 'fec_utils/itemized_parsers/committee_parse'
     require 'fec_utils/itemized_parsers/inter_committee_parse'
-    
   end
 end
