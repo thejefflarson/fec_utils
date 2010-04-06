9542a8785724f525df1b079463744bbc479f5095
Jeff Larson <thejefflarson@gmail.com>
2010-04-06 14:23:50 -0400

    Inter committee parse, looks the same as pas parse

---
+++ lib/fec_utils/itemized_parsers/inter_committee_parse.rb
@@ -0,0 +1,6 @@
+module FecUtils::ItemizedParsers
+  class InterCommitteeParse < FecUtils::ItemizedParsers::PasParse
+    # from ftp://ftp.fec.gov/FEC/oth_dictionary.txt
+    include FecUtils::ItemizedParsers::Base
+  end
+end
